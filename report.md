7c7f2538afb2966e5c75356d045037960e654943
SalmonTT <simontienz@gmail.com>
2021-03-29 11:27:27 +0800

    added price adaptive model

---
+++ report.md
@@ -0,0 +1,10 @@
+# Modling and Simulating Opinion Dynamics
+
+## Introductiojn
+
+
+
+## Background, Problem Statement, and Project Goals
+
+## Literature Review
+
--- simulation.py
+++ simulation.py
@@ -12,17 +12,29 @@ def BoundedConfidence(X, n, eps):
     C = np.zeros((n,n))
     for i in range(n):
         # count is the number of agents that are similar to agent i
-        # since {i} is included in I(i, x(t-1), p(t-1)), count is at least 1
         count = 0
         # for every agent, compare his/her opinion with others
         for j in range(n):
             if abs(X[i] - X[j]) <= eps[i]:
                 count += 1
                 C[i][j] = 1 # mark C[i][j] with 1 to show that they are similar
-        tmp = np.full(n, 1 / count)
-        C[i] = np.where(C[i] == 1, tmp, C[i]) # formula after eq.6
+        C[i] = np.where(C[i] == 1, 1/count, C[i]) # formula after eq.6
 
     return C
+
+def PriceAdaptive(X, P, n, eps):
+    # C is the update matrix
+    C = np.zeros((n, n))
+    for i in range(n):
+        # count is the number of agents whose opinions are closest to the actual price at time t
+        count = 0
+        for j in range(n):
+            if abs((P - X[j])/P) <= eps[i]:
+                count += 1
+                C[i][j] = 1  # mark C[i][j] with 1 to show that they are similar
+        C[i] = np.where(C[i] == 1, 1/count, C[i]) # formula after eq.6
+    return C
+
 
 def DoSimulation():
 
@@ -33,32 +45,39 @@ def DoSimulation():
     a = 0.1  # constant absolute risk aversion coefficient (CARA)
     var = 0.1  # variance of stock in risk premium
     z_s = 100  # supply per agent (assumed to be constant here under equilibrium condition)
-    alpha = np.full(n, .25) # alpha [0,1] is the update propensity parameter. Current it is set to be same across agents
-    eps = np.random.uniform(0, 0.4, n) # epsilon for BC model
-    print(eps)
+    alpha = np.random.uniform(0, 1, n)  # alpha [0,1] is the update propensity parameter.
+    eps_BC = np.random.uniform(0, 0.4, n) # epsilon for BC model
+    eps_PA = np.full(n, 0.1)
     X = np.random.uniform(1, 10, n) # X is X(t=0) which is the expected price for t=1 (next period)
-    A = np.identity(n) # alternatively, generate a network structure as initial A(t=0)
+    A = np.identity(n) # initialize A(t=0) as an identity matrix
+    # A = np.ones(n)  # initialize A(t=0) as an matrix full of ones
+
+    price_list = []
+
     # --- simulation --- #
     for round in range(t):
 
-        # obtain P(t)
+        X_prev = X
+        # --- obtain P(t), the equilibrium price on risky asset at time t ---
         P = PricingEq(X, r, a, var, z_s, n)
-        # obtain C(t)
-        C = BoundedConfidence(X, n, eps)
-        # obtain A(t)
+        price_list.append(P)
+        # --- obtain C(t) ---
+        # C = BoundedConfidence(X, n, eps_BC)
+        C = PriceAdaptive(X_prev, P, n, eps_PA)
+        # --- obtain A(t) ---
         A = alpha*C + (1-alpha)*A
-        # update X
+        # --- update X(round) -> X(round+1) ---
         X = np.matmul(A, X)
 
         print("Round ", round)
-        print("P: ", P)
-        print("A: ")
-        print(A)
-        print("C: ")
-        print(C)
+        # print("P: ", P)
+        # print("C: ")
+        # print(C)
+        # print("A: ")
+        # print(A)
         print("X: ")
         print(X)
-
+    print(price_list)
 
 DoSimulation()
 
