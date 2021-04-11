b31e2f6239a9c628fb419ab8d1013ccf1a1df44e
piperzeng <hongni7@126.com>
2021-04-11 13:06:59 +0800

    finish rational simulation

--- Rational_Network.py
+++ Rational_Network.py
@@ -6,9 +6,9 @@ def getDeltaZ(a, var, r, p, X):
     given the current price (p(t)) and current opinion (x(t), note x(t) is expected price for
     period t+1), we we obtain z(t) which is the optimal # of shares held for current period t.
     '''
-    print("price is", p)
-    print("this is X")
-    print(X)
+    # print("price is", p)
+    # print("this is X")
+    # print(X)
     # note that Z must be a discrete number
     deltaZ = (1/(a*var)) * (X - (1+r)*p) # equation 2
     deltaZ = deltaZ.astype(int)
@@ -120,16 +120,17 @@ def DoSimulation():
 
     # --- Initialization --- #
     n = 100  # number of agents
-    t = 100 # number of rounds
-    r = 0.0007700  # risk-free interest rate
+    t = 50 # number of rounds
+    r = 0.0008588  # risk-free interest rate
     a = 1  # constant absolute risk aversion coefficient (CARA)
     # beta = np.random.uniform(0, 10, n) # An array, risk preference when it comes to placing order
-    beta = np.random.uniform(0, 0.01, n)
-    price = 394.730011 # initialize p(t=0) to be price
-    var = 15.57  # variance of stock in risk premium
-    alpha = np.random.uniform(0, 1, n)  # alpha [0,1] is the update propensity parameter.
-    eps_BC = np.random.uniform(0, 0.01, n) # epsilon for BC model
-    X = np.random.normal(394.730011, 10, n)  # X is X(t=0) which is the expected price for t=1 (next period)
+    beta = np.random.uniform(0, 0.1, n)
+    price = 19.95 # initialize p(t=0) to be price
+    # var = 0.443208  # variance of stock in risk premium
+    var = 1.152757
+    alpha = np.random.uniform(0, 0.8, n)  # alpha [0,1] is the update propensity parameter.
+    eps_BC = np.random.uniform(0, 0.1, n)  # epsilon for BC model
+    X = np.random.normal(19.95, 3, n)  # X is X(t=0) which is the expected price for t=1 (next period)
     A = np.identity(n) # initialize A(t=0) as an identity matrix
     # A = np.ones(n)  # initialize A(t=0) as an matrix full of ones
 
@@ -138,8 +139,8 @@ def DoSimulation():
     orderPrice = np.zeros(n)  # prices of current order for each agent
     Z_current = np.random.randint(100, 500, n)  # each agent holds between 10 to 1000 shares
     print("sum of Z_current:", sum(Z_current))
-    print("initial Z_current:")
-    print(Z_current)
+    # print("initial Z_current:")
+    # print(Z_current)
     price_list_BC = []
     std_BC = [X.std()]
     # opinion_list_i = [X[10]]
@@ -147,8 +148,8 @@ def DoSimulation():
 
     # --- Simulation --- #
 
-    for round in range(t):
-        print("---------------- ROUND ", round, " ----------------")
+    for rd in range(t):
+
         # Portfolio holding dynamics
         Z_delta = getDeltaZ(a, var, r, price, X)
         actions = getAction(Z_delta, Z_current, actions, n)
@@ -158,55 +159,34 @@ def DoSimulation():
         price = updatePrice(Z_delta, actions, orderPrice, n)
         # Expected price dynamics
         C = updateXwithBC(X, n, eps_BC)
-        # print("C")
-        # for i in range(n):
-        #     print(sum(C[i]))
-        # C = updateXwithPA(X_prev, P, n, eps_PA)
-        # A = alpha*C + (1-alpha)*A
-        # print("alpha")
-        # print(alpha)
-        # print(alpha.shape)
-        # print("C")
-        # print(C)
-        # print("1-alpha")
-        # print(1-alpha)
-        # tmp = alpha * C
-        # print("tmp", tmp.shape)
-        # print(tmp)
-        # tmp = np.zeros((n, n))
         for i in range(n):
             for j in range(n):
                 # tmp[i][j] = alpha[i]*C[i][j]
                 A[i][j] = alpha[i]*C[i][j] + (1-alpha[i])*A[i][j]
-        # print("tmp")
-        # print(tmp)
-        # print("A")
-        # print(A)
-        # A = alpha * C + (1 - alpha) * np.identity(n)
-        # print("A")
-        # for i in range(n):
-        #     print(sum(A[i]))
-        # A = alpha*C + (1-alpha)*(np.identity(n))
         X = np.matmul(A, X)
-
-        # opinion_list_i.append(X[10])
 
 
         price_list_BC.append(price)
         std_BC.append(X.std())
 
-        print("this is Z_delta:")
-        print(Z_delta)
-        print("actions are:")
-        print(actions)
-        print("this is Z_current after change:")
-        print(Z_current)
-        print("Order prices: ", orderPrice)
-        print("price for next period: ", price)
+        # if (rd %10==0) :
+        #     print("---------------- ROUND ", rd, " ----------------")
+        #     print("this is Z_delta:")
+        #     print(Z_delta)
+        #     print("actions are:")
+        #     print(actions)
+        #     print("this is Z_current after change:")
+        #     print(Z_current)
+        #     print("Order prices: ", orderPrice)
+        #     print("price for next period: ", price)
 
     print("sum of Z_current:", sum(Z_current))
     print(price_list_BC)
+    print(price_list_BC[0], price_list_BC[-1])
     print(std_BC)
     # print(opinion_list_i)
-DoSimulation()
+
+for i in range(5):
+    print("round ", i)
+    DoSimulation()
 
