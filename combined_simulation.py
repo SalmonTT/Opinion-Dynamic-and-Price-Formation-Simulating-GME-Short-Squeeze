43d01eea1a183d1d7b9c76a0e57b3bc7968ef13f
SalmonTT <simontienz@gmail.com>
2021-04-11 16:51:30 +0800

    updated combined

--- combined_simulation.py
+++ combined_simulation.py
@@ -8,8 +8,8 @@ def getDeltaZ(a, var, r, p, X):
     period t+1), we we obtain z(t) which is the optimal # of shares held for current period t.
     '''
     print("price is", p)
-    print("this is X")
-    print(X)
+    # print("this is X")
+    # print(X)
     # note that Z must be a discrete number
     deltaZ = (1 / (a * var)) * (X - (1 + r) * p)  # equation 2
     deltaZ = deltaZ.astype(int)
@@ -138,7 +138,7 @@ def getOrderPriceSimple(Z_delta, r, p, orderPconstant):
     '''
     orderPrice = np.zeros(len(Z_delta))
     for i in range(len(Z_delta)):
-        orderPrice[i] = (1 + r + orderPconstant) * p
+        orderPrice[i] = random.uniform((1+r)*p, (1+r)*p*(1+orderPconstant))
     return orderPrice
 
 
@@ -152,15 +152,15 @@ def getOrderPriceComplex(orderPrice, n, r, X, p):
 def DoSimulation():
     '''Rational Network Initialization'''
     n = 100  # number of agents
-    t = 20  # number of rounds
-    r = 0.0007700  # risk-free interest rate
+    t = 50  # number of rounds
+    r = 0.0008588  # risk-free interest rate
     a = 1  # constant absolute risk aversion coefficient (CARA)
-    beta = np.random.uniform(0, 10, n)  # An array, risk preference when it comes to placing order
-    price = 100  # initialize p(t=0) to be price
-    var = 1  # variance of stock in risk premium
-    alpha = np.random.uniform(0, 1, n)  # alpha [0,1] is the update propensity parameter.
+    beta = np.random.uniform(0, 0.1, n)  # An array, risk preference when it comes to placing order
+    price = 19.95  # initialize p(t=0) to be price
+    var = 1.152757  # variance of stock in risk premium
+    alpha = np.random.uniform(0, 0.8, n)  # alpha [0,1] is the update propensity parameter.
     eps_BC = np.random.uniform(0, 0.1, n)  # epsilon for BC model
-    X = np.random.normal(100, 10, n)  # X is X(t=0) which is the expected price for t=1 (next period)
+    X = np.random.normal(19.95, 3, n)  # X is X(t=0) which is the expected price for t=1 (next period)
     A = np.identity(n)  # initialize A(t=0) as an identity matrix
     actions = np.zeros(n)  # current actions for each agent (discrete values of 1, -1 and 0 - Buy Sell Hold)
     order_price_rational = np.zeros(n)  # prices of current order for each agent
@@ -174,10 +174,9 @@ def DoSimulation():
     '''Irrational Network Initialization'''
     Z_current_irrational = [] # length of this list indicates the number of total irrational agents in the network
     Z_delta_irrational = []
-    max_Z_delta = 20
-    order_price_irrational = []  # prices of current order for each agent
-    irrational_orderprice_alpha = 0.05
-    GME_price_array = [14927612
+    max_Z_delta = 100
+    orderPconstant = 0.1
+    GME_volume_array = [14927612
                     ,7060665
                     ,144501736
                     ,93717410
@@ -197,10 +196,9 @@ def DoSimulation():
                     ,62427275
                     ,81345013]
     # number of agents added at each round proportional to the volume traded that day (Jan 11 - Feb 5)
-    add_agents_sequence = [x/10000000 for x in GME_price_array]
+    add_agents_sequence = [x/10000000 for x in GME_volume_array]
     add_agents_sequence = [int(x) for x in add_agents_sequence]
     print(add_agents_sequence)
-
 
     # --- Simulation --- #
 
@@ -234,10 +232,7 @@ def DoSimulation():
 
         # Price dynamics (takes into consideration both rational and irrational agents orders
         order_price_rational = getOrderPrice(actions, beta, price, X, order_price_rational, n, r)
-        order_price_irrational = getOrderPriceSimple(Z_delta_irrational, r, price, irrational_orderprice_alpha)
-        print("orderprices")
-        print(len(order_price_rational))
-        print(len(order_price_irrational))
+        order_price_irrational = getOrderPriceSimple(Z_delta_irrational, r, price, orderPconstant)
         price = updatePrice(Z_delta_rational, Z_delta_irrational, actions, order_price_rational, order_price_irrational)
 
 
@@ -261,6 +256,6 @@ def DoSimulation():
         # print(Z_current)
         # print("Order prices: ", orderPrice)
         print("price for next period: ", price)
-
+    print(price_list_Rational)
 
 DoSimulation()
